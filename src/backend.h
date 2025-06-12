69f19be6b684c0da7940c17a7fe688efaed46c58
Changwoo Yu <changwooyu9@gmail.com>
2025-06-12 11:49:41 -0500

    refactoring

--- src/backend.h
+++ src/backend.h
@@ -30,6 +30,47 @@ namespace PTY {
 		char keycode;
 		vector<char> ansicode;
 	};
-
 	void GetPtyData(PtyData *data, char b, vector<char> *tmp, bool *ESC, bool *CSI);
+}
+
+/* 
+	The Grid consists of many Cells. As of now, it is a two dimensional vector
+	of Cells. It made the most sense to have it be represented as a two dimensional
+	array with all the cursor movements. 
+
+	Would it be possible to implement it using one dimensional vector? Maybe? but
+	I think it would unnecessarily complicate the cursor movements...
+
+	Grid is what handles cursor movements, scrolling, and other tasks related to 
+	rendering. Grid should set up its parameters just right, so that the Terminal
+	can just "read off" of Grid and just draw characters
+*/
+namespace GRID {
+	using namespace PTY;
+	typedef struct Cell Cell; 
+	/*
+		For now, the Cell only contains information regarding the keycode and 
+		whether the current cell is a continuation of the previous line, but it 
+		will get more complicated as I add more features (Are emojis too much to ask?)
+
+		The Terminal should not directly "see" indiviual cells!
+	*/
+	struct Cell {
+		char keycode; 
+		bool lineBreak = false;
+	};	
+
+	class Grid {
+	public:
+		Grid();
+		void ParseAnsiCode(PtyData *ansi);
+		void SetGridElement(PtyData *data);
+		void AddNewLine();
+		void ResizeGrid(int rows, int cols);
+	private:
+		vector<vector<Cell>> grid;	 
+		int gridHeight, gridWidth; 
+		int cursorX, cursorY;
+		int rowScroll;
+	};	
 }	
